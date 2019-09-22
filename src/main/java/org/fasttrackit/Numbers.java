c5706b5b9f8ceef206b8c6d92dd3e5d7ac202cb1
andibabett <andi_babett@yahoo.com>
2019-09-22 17:08:41 +0300

    Initial commit

---
+++ src/main/java/org/fasttrackit/Numbers.java
@@ -0,0 +1,18 @@
+package org.fasttrackit;
+
+public class Numbers {
+
+    int number;
+
+
+    public Numbers(int number1) {
+
+        public double Sumary(int number2,int number1)  {
+            Sumary = number1 + number2;
+
+            System.out.println("Summary for number1 and number2 is" + Sumary);
+        }
+
+
+    }
+    }
---
+++ src/main/java/org/fasttrackit/RetairmentCalculator.java
@@ -0,0 +1,27 @@
+package org.fasttrackit;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class RetairmentCalculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("What is your current age?");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+
+        System.out.println("At what age would you like to retire?");
+        int retairementAge = scanner.nextInt();
+
+        LocalDate currentDate = LocalDate.now();
+        int remainingYears = retairementAge - age;
+
+        System.out.println("You have" + remainingYears + "years left until retirement.");
+        System.out.println("It's" + currentDate.getYear() + ", so you can retaire in" +
+                (currentDate.getYear() + remainingYears));
+
+
+
+    }
+    }

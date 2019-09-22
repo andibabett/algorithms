ef9ad9b3945594f879919d7d06bb75b0be40af97
andibabett <andi_babett@yahoo.com>
2019-09-22 17:09:42 +0300

    summary

--- src/main/java/org/fasttrackit/Numbers.java
+++ src/main/java/org/fasttrackit/Numbers.java
@@ -7,7 +7,7 @@ public class Numbers {
 
     public Numbers(int number1) {
 
-        public double Sumary(int number2,int number1)  {
+         public double Sumary(int number2,int number1)  {
             Sumary = number1 + number2;
 
             System.out.println("Summary for number1 and number2 is" + Sumary);
